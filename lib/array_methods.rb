94500451e7b14cfa6c0f8acd7b584772b2904893
nbyrd9 <nianibyrd@gmail.com>
2021-01-13 10:39:30 -0500

    commit tree 354ceda51902dd9fc630958b6368d62118fb7210

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,28 @@
 def using_include(array, element)
-
+    array.include?(element)
 end
 
 def using_sort(array)
-
+    array = ["wow" , "I" , "arrays!"]
+    array.sort
 end
 
 def using_reverse(array)
-
+    array = ["wow" , "I" , "am" , "arrays!"]
+    array.reverse
 end
 
 def using_first(array)
-
+    array = ["wow" , "I" , "am" , "arrays!"]
+    array.first
 end
 
 def using_last(array)
-
+    array = ["wow" , "I" , "am" , "arrays!"]
+    array.last
 end
 
 def using_size(array)
-
+    array = ["wow" , "I" , "am" , "learning" , "much" , "arrays!"]
+    array.size
 end
